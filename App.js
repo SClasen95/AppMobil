a3b50988b689abd31c4192abfd41a67754ac383b
SClasen95 <simonclasen1@outlook.com>
2020-11-28 17:30:49 -0300

    Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {isLoggedIn:true}
+    this.state = {isLoggedIn:false}
 }
   setLogin(){
     this.setState({isLoggedIn:true});
